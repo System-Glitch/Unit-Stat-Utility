70dd86a2769afa2c33c0f8b34d1091cf25a02c5f
SystemGlitch <jeremy.la@outlook.fr>
2019-08-07 15:39:52 +0200

    Import advisors

---
+++ gamedata/build_advisors.js
@@ -0,0 +1,65 @@
+const API_URL    = 'https://api.projectceleste.com/gamedb'
+const IMAGES_URL = 'https://images.projectceleste.com/Art/'
+
+const fs      = require('fs')
+const parser  = require('fast-xml-parser')
+const helpers = require('./helpers.js')
+
+const techtreeXML  = fs.readFileSync('./gamedata/techtreex.xml').toString()
+const options = {
+    attributeNamePrefix : "",
+    attrNodeName: "attr", //default is 'false'
+    textNodeName : "#text",
+    ignoreAttributes : false,
+    ignoreNameSpace : false,
+    allowBooleanAttributes : true,
+    parseNodeValue : true,
+    parseAttributeValue : true,
+    trimValues: true,
+    cdataTagName: "__cdata", //default is 'false'
+    cdataPositionChar: "\\c",
+    localeRange: "", //To support non english character in tag/attribute values.
+    parseTrueNumberOnly: false,
+}
+const stringsXML   = fs.readFileSync('./gamedata/stringtablex.xml').toString()
+const language  = parser.parse(stringsXML, options).stringtable.language.string
+const techtree  = parser.parse(techtreeXML, options).TechTree.Tech
+
+const result = [{}, {}, {}, {}]
+
+helpers.get(API_URL + '/advisors', (data) => {
+	const advisors = JSON.parse(data)
+
+	for(let key in advisors) {
+		const advisor = advisors[key]
+		const icon = advisor.icon.replace(/\\/g, '/') + '.png'
+
+		helpers.downloadImage(IMAGES_URL + icon, './img/Advisors/' + advisor.name  + '.png')
+
+		const tech = helpers.findByAttributeValue(techtree, 'name', advisor.techs.tech)
+		const effects = []
+
+		const effect = tech.Effects ? tech.Effects.Effect : undefined
+        if(Array.isArray(effect)) {
+            for(let keyEffect in effect) {
+                const ef = effect[keyEffect]
+                const cef = helpers.convertEffect(ef)
+                if(cef) effects.push(cef)
+            }
+        } else {
+        	const cef = helpers.convertEffect(effect)
+            if(cef) effects.push(cef)
+        }
+
+		const entry = {
+			name: helpers.findByAttributeValue(language, '_locid', advisor.displaynameid)['#text'],
+			description: helpers.findByAttributeValue(language, '_locid', advisor.displaydescriptionid)['#text'],
+			rarity: helpers.rarityToInteger(advisor.rarity),
+			civ: advisor.civilization,
+			effects: effects
+		}
+		result[advisor.age][advisor.name] = entry
+	}
+
+	helpers.save(result, './scripts/advisors.json')
+})
---
+++ gamedata/unit_types.js
@@ -0,0 +1,25 @@
+const API_URL = 'https://api.projectceleste.com/gamedb'
+const types   = ["AbstractCavalry","UnitTypeBldgTower","UnitTypeShipUtility1","AbstractWall","AbstractInfantry","UnitTypeBldgTownCenter","UnitTypeBldgWonder","AbstractPriest","UnitTypeArcherAntiInf1","UnitTypeCavalryBasic1","UnitTypeCaravan1","UnitTypeVillager1","UnitTypeSiegeTrebuchet1","UnitTypeBldgFortress","Ship","UnitTypeSiegeBallista1","AbstractArcher","AbstractCavalry","AbstractTownCenter","AbstractArtillery","UnitTypeBldgBarracks","AbstractFarm","UnitTypeShipFishing1","UnitTypeScout1","UnitTypeBldgHouse","UnitTypeBldgStorehouse","UnitTypeInfantryAntiCav1","Building","AbstractVillager"]
+
+const fs      = require('fs')
+const helpers = require('./helpers.js')
+
+const unitsRaw = fs.readFileSync('./scripts/units.js', 'utf8')
+const units = JSON.parse(unitsRaw.replace('var units=', '').replace(';', ''))
+
+helpers.get(API_URL + '/units', (data) => {
+    const dataUnits = JSON.parse(data)
+    for(let key in units) {
+        const unit = units[key]
+        const model = helpers.findUnit(dataUnits, key)
+
+        unit.types = []
+        for(let keyType in model.UnitType) {
+            if(types.indexOf(model.UnitType[keyType]) != -1) {
+                unit.types.push(model.UnitType[keyType])
+            }
+        }
+    }
+    fs.writeFileSync('./scripts/units.js', 'var units=' + JSON.stringify(units) + ';')
+})
+
